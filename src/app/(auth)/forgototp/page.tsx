7b677c1fcb2efb605a834d8cefa7171f4337f936
Akhil-Kumar-2001 <akhilkumars2001@gmail.com>
2025-05-01 16:18:47 +0530

    fix:wrapper added in forgot password page.

--- src/app/(auth)/forgototp/page.tsx
+++ src/app/(auth)/forgototp/page.tsx
@@ -1,7 +1,10 @@
 import ForgotOtpVerification from "@/components/forgototp";
+import { Suspense } from 'react';
 
-const OtpPage = () =>{
-  return <ForgotOtpVerification role="student"  />
+const OtpPage = () => {
+  return (<Suspense fallback={<div>Loading...</div>}>
+    <ForgotOtpVerification role="student" />
+  </Suspense>)
 }
 
 export default OtpPage;
--- src/app/tutor/forgototp/page.tsx
+++ src/app/tutor/forgototp/page.tsx
@@ -1,7 +1,20 @@
-import ForgotOtpVerification from "@/components/forgototp";
+// import ForgotOtpVerification from "@/components/forgototp";
 
-const OtpPage = () =>{
-  return <ForgotOtpVerification role="tutor"  />
+// const OtpPage = () =>{
+//   return <ForgotOtpVerification role="tutor"  />
+// }
+
+// export default OtpPage;
+
+
+
+import ForgotOtpVerification from "@/components/forgototp";
+import { Suspense } from 'react';
+
+const OtpPage = () => {
+  return (<Suspense fallback={<div>Loading...</div>}>
+    <ForgotOtpVerification role="tutor" />
+  </Suspense>)
 }
 
 export default OtpPage;
