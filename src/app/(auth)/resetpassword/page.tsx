67b86445bfa48b74a4d6832e485bc8949ecb5416
Akhil-Kumar-2001 <akhilkumars2001@gmail.com>
2025-05-01 16:36:20 +0530

    fix:wrapper added in reset password

--- src/app/(auth)/resetpassword/page.tsx
+++ src/app/(auth)/resetpassword/page.tsx
@@ -1,7 +1,10 @@
 import ResetPasswordPage from "@/components/resetpassword";
+import { Suspense } from "react";
 
 const ResetPassword = () => {
-    return<ResetPasswordPage role="student" />
+    return (<Suspense fallback={<div>Loading...</div>}>
+        <ResetPasswordPage role="student" />
+    </Suspense>)
 }
 
 export default ResetPassword;
