49fe7ceb4f9ff14777ac7d60dd2b82470f51efef
Akhil-Kumar-2001 <akhilkumars2001@gmail.com>
2025-05-07 13:18:01 +0530

    midddleware check 3

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,7 +11,6 @@ export async function middleware(req: NextRequest) {
     console.log("Skipping middleware for login request...");
     return NextResponse.next();
   }
-  console.log("Cookie from  mdidlleware",req.cookies.get('accessToken'));
   const token = req.cookies.get('accessToken')?.value || req.cookies.get('admin-accessToken')?.value;
   const refreshToken = req.cookies.get('refreshToken')?.value; 
   const adminRefreshToken = req.cookies.get('admin-refreshToken')?.value;
@@ -28,6 +27,8 @@ export async function middleware(req: NextRequest) {
 
   console.log("middleware role =>", role);
 console.log("tokern from middleware",token)
+console.log("Cookie from  mdidlleware",req.cookies.get('accessToken'));
+
   if (!token) {
     if (pathname.startsWith('/tutor')) {
       return NextResponse.redirect(new URL('/tutor/login', req.url));
